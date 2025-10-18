4efb21876eae65c73de346d79bf4dea76ef25e92
KazoonKa <khazoonka@gmail.com>
2025-10-18 22:42:11 +0700

    Add files via upload

---
+++ bsod.bat
@@ -0,0 +1,16 @@
+@echo off
+echo # KazoonKa's Force BSOD #
+echo !! USE AT YOUR OWN RISK !!
+echo !! I AM NOT RESPONSIBLE FOR ANY DAMAGES CAUSED BY THIS PROGRAM !!
+echo Close this program immediately if you don't want your PC in risk.
+pause
+echo are you sure? Press any key to continue
+timeout /t -1 >nul
+echo ARE YOU SURE??!?!?!??
+timeout /t -1 >nul
+echo REALLY???!?!?!?!?!?
+timeout /t -1 >nul
+echo You decided it...
+echo If you are ready, Press any key to BSOD.
+timeout /t -1 >nul
+powershell wininit
