63e24f3f3c5bcdeea5a14949c4399a624294ea84
KazoonKa <khazoonka@gmail.com>
2025-10-18 22:13:51 +0700

    Add files via upload

---
+++ hackstyle.bat
@@ -0,0 +1,57 @@
+@echo off
+title DATA BREACHER V 1.1
+color a
+echo ###########################
+echo # K'S DATA BREACHER V 1.1 #
+echo ###########################
+echo 
+pause
+echo initializing systems...
+timeout /t 2 /nobreak >nul
+echo loading breacher.bat
+timeout /t 2 /nobreak >nul
+echo loading patcher.bat
+timeout /t 2 /nobreak >nul
+echo loading maincontroller.exe
+timeout /t 2 /nobreak >nul
+echo searching for targets...
+timeout /t 2 /nobreak >nul
+echo found 1 : COM-1
+echo only detected one possible target
+pause
+tree \
+echo ---
+echo data successfully breached
+echo press any key to load patcher
+timeout /t -1 >nul
+echo initializing patcher.bat
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 1 /nobreak >nul
+echo patching C:\users\COM-1\.data\%random%%random%%random%%random%%random%%random%%random%
+timeout /t 3 /nobreak >nul
+echo patching done
+echo press any key to exit safely
+timeout /t -1 >nul
